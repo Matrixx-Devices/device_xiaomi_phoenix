295f8ac6d08dedb9cc94512afbc8ed25e5a54598
Jay Mistry <jaymistry258@gmail.com>
2023-10-09 05:25:31 +0000

    phoenix: rearrange stuff
    Signed-off-by: Jay Mistry <jaymistry258@gmail.com>

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -5,15 +5,15 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, device/xiaomi/phoenix/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/xiaomi/phoenix/device.mk)
-
-# Camera
-$(call inherit-product-if-exists, device/xiaomi/phoenix-miuicamera/config.mk)
 
 # Inherit some common AlphaDroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit Miui Camera.
+$(call inherit-product-if-exists, device/xiaomi/phoenix-miuicamera/config.mk)
 
 # AlphaDroid Build Type & Maintainer stuff.
 ALPHA_MAINTAINER := JAYMISTRY258
@@ -22,36 +22,50 @@ ALPHA_BUILD_TYPE := OFFICIAL
 # SOC
 PROCESSOR_MODEL := SM6150
 
-# Gapps Build Details.
-TARGET_GAPPS_ARCH := arm64
-
 # GAPPS Build Details.
 WITH_GAPPS := true
-TARGET_INCLUDE_GOOGLE_APP := true
-TARGET_INCLUDE_GOOGLE_CALCULATOR := true
-TARGET_INCLUDE_GOOGLE_CALENDAR := true
-TARGET_INCLUDE_GOOGLE_CHROME := true
-TARGET_INCLUDE_GOOGLE_DESKCLOCK := true
-TARGET_INCLUDE_GOOGLE_DRIVE := true
+TARGET_GAPPS_ARCH := arm64
+
+# Included Gapps Packages.
 TARGET_INCLUDE_GMAIL := true
+TARGET_INCLUDE_GBOARD := true
+TARGET_INCLUDE_GOOGLE_APP := true
+TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_INCLUDE_GOOGLE_MAPS := true
-TARGET_INCLUDE_GOOGLE_PHOTOS := true
 TARGET_INCLUDE_GOOGLE_SETUP := true
+TARGET_INCLUDE_ANDROID_AUTO := true
+TARGET_INCLUDE_GOOGLE_CLOCK := true
+TARGET_INCLUDE_GOOGLE_CAMERA := false
+TARGET_INCLUDE_GOOGLE_PHOTOS := true
+TARGET_INCLUDE_GOOGLE_DIALER := true
+TARGET_INCLUDE_GOOGLE_CHROME := true
+TARGET_INCLUDE_GOOGLE_MARKUP := true
+TARGET_INCLUDE_GOOGLE_SOUNDS := true
+TARGET_INCLUDE_GOOGLE_CONTACTS := true
+TARGET_INCLUDE_GOOGLE_CALENDAR := true
+TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_GOOGLE_MESSAGING := true
+TARGET_INCLUDE_GOOGLE_CALCULATOR := true
+TARGET_INCLUDE_GOOGLE_FILE_MANAGER := true
+TARGET_INCLUDE_GOOGLE_BATTERY_STATS := true
 
 # Custom stuff.
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := false
+TARGET_USE_GOOGLE_TELEPHONY := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BUILD_APERTURE_CAMERA := false
-TARGET_USE_PIXEL_FINGERPRINT := false
 TARGET_INCLUDE_PIXEL_CHARGER := false
-TARGET_USE_GOOGLE_TELEPHONY := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_ENABLE_BLUR := true
+
+# Debugging stuff.
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2400
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # GMS
@@ -59,7 +73,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_phoenix
-PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
+PRODUCT_DEVICE := phoenix
 PRODUCT_MANUFACTURER := Xiaomi
